a9ff76e08a2332cdf49c51d36dca8ba1af22a2ed
Bachan Kubi <felixghagra@gmail.com>
2024-06-27 00:43:50 -0700

    todo form added

--- react-ts/src/Components/Todo/TodoForm.tsx
+++ react-ts/src/Components/Todo/TodoForm.tsx
@@ -1,14 +1,28 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TodoContext } from "../../Context/TodoProvider";
 
 
 const TodoForm = () => {
-    const {todoTitle} = useContext(TodoContext);
-    console.log(todoTitle)
+    const { state, dispatch} = useContext(TodoContext);
+    console.log(state, 'todoState');
+    const [task, setTask] = useState("");
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const todo = {
+            id: Math.floor(Math.random() * (1000 - 1)) + 1,
+            title: "task",
+            isCompleted: false
+        }
+        dispatch({type: "addTodo", payload: todo})
+    }
     return (
         <div>
-            <h1>{todoTitle}</h1>
-            <h3>Todo Form</h3>
+            <h1>Add ToDo</h1>
+            <form>
+                <label htmlFor="todo">Task</label>
+                <input type="text" id="todo" name="todo" onBlur={(e)=>setTask(e.target.value)}/>
+                <button onClick={handleSubmit} type="submit">Add</button>
+            </form>
         </div>
     );
 };
